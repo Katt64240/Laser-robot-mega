6e0c5634ddc69b759149a933c635819aa0ebf704
Katt64240 <58326215+Katt64240@users.noreply.github.com>
2019-12-06 19:04:00 +0100

    Demo mechanics

---
+++ pussel spel/Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour {
+
+	public float walkSpeed = 4.0f;
+	public float mouseSpeed = 3.0f;
+	public float verticalRange = 90f;
+	public float gravity = -9.81f;
+	public float jumpSpeed = 10.0f;
+	public GameObject cameraCenter;
+
+
+	float verticalRotation;
+	Vector3 moveDirection;
+	float forwardSpeed;
+	float sideSpeed;
+	float moveSpeed;
+	float ySpeed;
+
+	CharacterController cc;
+
+	// Use this for initialization
+	void Start () { 
+		cc = GetComponent<CharacterController>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		ySpeed += gravity * Time.deltaTime;
+		if (cc.isGrounded) {
+			ySpeed = -1;
+			if (Input.GetKey (KeyCode.Space)) {
+				ySpeed = jumpSpeed;
+			}
+		}
+
+		//Rotaion
+		transform.Rotate(Vector3.up * Input.GetAxis ("Mouse X") * mouseSpeed);
+		verticalRotation += Input.GetAxis("Mouse Y") * mouseSpeed;
+		verticalRotation = Mathf.Clamp(verticalRotation, -verticalRange, verticalRange);
+		cameraCenter.transform.localEulerAngles = Vector3.left * verticalRotation;
+
+
+		moveSpeed = walkSpeed;
+		forwardSpeed = Input.GetAxis ("Vertical") * moveSpeed;
+		sideSpeed = Input.GetAxis ("Horizontal") * moveSpeed;
+
+		moveDirection = new Vector3 (sideSpeed, ySpeed, forwardSpeed);
+		moveDirection = transform.rotation * moveDirection;
+
+        cc.Move(moveDirection * Time.deltaTime);
+	}
+}
+
---
+++ pussel spel/Assets/Scripts/LaserBeam.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeam : MonoBehaviour
+{
+
+    public bool IsActivated = true;
+
+    LineRenderer lr;
+
+    float maxRayDistance = 1000f;
+    float rayWidth = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>(); // använd egen cylinder mesh class
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsActivated)
+        {
+            UpdateLine();
+        }
+    }
+
+    void UpdateLine()//uppdatera med mer funktionalitet
+    {
+        List<Vector3> points = new List<Vector3>();
+        points.Add(transform.position);
+
+
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,transform.forward,out hit, maxRayDistance)) // använd spherecast istället
+        {
+            points.Add(hit.point);
+        }
+        else
+        {
+            points.Add(transform.position+transform.forward* maxRayDistance);
+        }
+
+        lr.positionCount = points.Count;
+        lr.SetPositions(points.ToArray());
+
+        lr.SetWidth(rayWidth, rayWidth);
+    }
+}
