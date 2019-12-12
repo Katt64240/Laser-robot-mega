d37994ddc44ce1191dd5e0bf3f80533ab429ef6d
Katt64240 <58326215+Katt64240@users.noreply.github.com>
2019-12-12 10:04:53 +0100

    Beam programing

---
+++ pussel spel/Assets/Scripts/Laser Beam/BeamMesh.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BeamMesh
+{
+    private static int lod = 4; //faces per cylinder
+    private static float beamRadious = 0.1f;
+
+    
+    public static Mesh (BeamSegment[] segments, Transform origo)
+    {
+        // use origo.inversTransformPoint
+
+
+    }
+
+}
---
+++ pussel spel/Assets/Scripts/Laser Beam/BeamSegment.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct BeamSegment
+{
+    public Vector3 p1;
+    public Vector3 p2;
+
+    public BeamSegment(Vector3 p1, Vactor3 p2)
+    {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+}
