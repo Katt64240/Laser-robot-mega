1eaa2b5814781967f96647e5e6b259d0e6d6e830
Katt64240 <58326215+Katt64240@users.noreply.github.com>
2020-02-06 09:50:31 +0100

    Added audio manager

---
+++ pussel spel/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioClip[] soundtracks;
+
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    int currentSound = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            int nextSound = Random.Range(0, soundtracks.Length);
+            audioSource.clip = soundtracks[nextSound];
+            audioSource.Play(0);
+        }
+    }
+}
