df8a26eca3d2334696149115e72723744385ab29
Katt64240 <58326215+Katt64240@users.noreply.github.com>
2020-03-26 10:11:17 +0100

    bugfix + demo 2

---
+++ pussel spel/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        timer += Time.deltaTime;
+    }
+
+    public void LoadLevel(int i)
+    {
+        Application.LoadLevel(i);
+    }
+
+    public void StartTimer()
+    {
+        timer = 0;
+    }
+}
---
+++ pussel spel/Assets/Scripts/StatusDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusDisplay : MonoBehaviour
+{
+    public Text text;
+    public bool update = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = "Time : " + GameManager.timer.ToString("#.00");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (update)
+        {
+            text.text = "Time : " + GameManager.timer.ToString("#.00");
+        }
+    }
+}
